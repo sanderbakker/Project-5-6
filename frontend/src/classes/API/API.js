b4fa4c4c7be6711803f10c8a8f0e62f1fd0e4b4d
Lennart Voogd <lennartvoogd@live.nl>
2017-10-24 23:12:42 +0200

    Comment blocks for API base class

--- frontend/src/classes/API/API.js
+++ frontend/src/classes/API/API.js
@@ -1,13 +1,19 @@
-/* 
+/** 
  * Base class for wrapper backend API
  * Provides the low level api calls. 
- *
-*/
+ */
 
 class API {
 	constructor() {
 		this.API_url = 'http://localhost:5000/api'
 	} 
+
+	/**
+	 * Sends POST request to API.
+	 *
+	 * var _endpoint  - (String) API endpoint (part after the base URL). 
+	 * var _data 	  - (Array) array that will be send to the server.  
+	 */
 
 	post(_endpoint, _data) {
 		return fetch(this.API_url + _endpoint, { 
@@ -20,9 +26,16 @@ class API {
 		});	
 	}
 
+	/**
+	 * Sends GET request to API.
+	 *
+	 * var _endpoint  - (String) API endpoint (part after the base URL). 
+	 */	
+
 	get(_endpoint) {
 		return fetch(this.API_url + _endpoint); 
 	}
 }
 
+// Export API for global use
 export { API }

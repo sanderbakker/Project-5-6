13a7aa5776e7e5a852837ef442dab043e028363c
Lennart Voogd <lennartvoogd@live.nl>
2017-11-21 11:39:34 +0100

    Removed searchProdcutByName method from interface

--- API/Services/IProductRepository.cs
+++ API/Services/IProductRepository.cs
@@ -12,7 +12,6 @@ namespace API.Services
         IEnumerable<string> GetCategories();
         IEnumerable<Product> GetLatest(int size); 
         IEnumerable<Product> GetFiltered(string name, string sort, int index, int size); 
-        IEnumerable<Product> SearchByName(string searchString); 
         int GetAmount();
     }
 }

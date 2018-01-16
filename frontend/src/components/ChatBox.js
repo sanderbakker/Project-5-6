3d1abb0f2219bdc5f407e73ba0348785b983688d
Dimitriphilips <Dimitriphilips@msn.com>
2018-01-16 22:58:49 +0100

    Added Chat to Chatbox

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
+import io from 'socket.io-client'
+import {USER_CONNECTED, LOGOUT} from '../chat-server/Events.js'
+import ChatLogin from './ChatLogin'
+import ChatContainer from './ChatContainer'
+import ChatSideBar from './ChatSideBar'
 
+const socketUrl = "http://localhost:3231" 
 
 export default class ChatBox extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            toggled: false
+            toggled: false,
+            socket: null,
+            user: null
         }
+    }
+
+    componentWillMount() {
+        this.initSocket()
+    }
+
+    initSocket = ()=>{
+        const socket = io(socketUrl)
+        socket.on('connect', ()=>{
+            console.log("Connected");
+        })
+        this.setState({socket})
+    }
+
+    setUser = (user)=>{
+        const {socket} = this.state
+        socket.emit(USER_CONNECTED, user);
+        this.setState({user})
+    }
+
+    logout = ()=>{
+		const { socket } = this.state
+		socket.emit(LOGOUT)
+		this.setState({user:null})
     }
 
     toggle() {
@@ -21,14 +53,21 @@ export default class ChatBox extends Component {
     }
 
     render() {
+        const { title } = this.props
+        const { socket, user } = this.state
         return (
             <div className={"chatbox"} >
-               <div onClick={f => this.toggle()} className={"chatbox-header"}>
-                   <i className={"fa  fa-commenting-o"}></i><span> Live chat</span>
-               </div>
-               <div id="chatbox-toggle" className={"chatbox-content display-none"}>
-                    
-               </div>
+                <div onClick={f => this.toggle()} className={"chatbox-header"}>
+                    <i className={"fa  fa-commenting-o"}></i><span> Live chat</span>
+                </div>
+                    <div id="chatbox-toggle" className={"chatbox-content display-none"}>
+                    {
+                        !user ?
+                        <ChatLogin socket={socket} setUser={this.setUser} />
+                        :
+                        <ChatContainer socket={socket} user={user} logout={this.logout} />
+                    }
+                    </div>
             </div>
         );
     }
